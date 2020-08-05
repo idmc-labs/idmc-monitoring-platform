1f81db141ab4ece47d539cec266a4089e4feb63a
annshress <ann.shress@gmail.com>
2020-08-05 12:19:55 +0545

    modify feed.runner to push obtained feed into db

--- feeds/collectors/gdacs.py
+++ feeds/collectors/gdacs.py
@@ -110,7 +110,7 @@ class GDACSFeed():
 
     @classmethod
     def format_string_to_ISO_date(cls, date: str) -> str:
-        return parse(date).strftime('%Y-%m-%d')
+        return parse(date).date()
 
     def format_date_features(self, items: List) -> List:
         return list(map(lambda item: {
--- feeds/connectors/gdacs.py
+++ feeds/connectors/gdacs.py
@@ -1,24 +1,26 @@
-import json
+from datetime import date
 
+from dateutil.parser import parse
 from sqlalchemy import create_engine, desc, asc
 
 from feeds.collectors.gdacs import GDACSFeed
-from feeds.connectors import session
-from feeds.tables.gdacs import Gdacs
+from feeds.connectors import session, engine
+from feeds.tables import GdacsTable
 
 
 def runner():
-    limit = 100
     # data intersection is checked against following fields
-    fields = ('publisheddate', 'id')
-    print(f'Fetching latest {limit} GDACS entries')
+    print(f'Fetching existing GDACS entries', end='\n\n')
     latest_100_entries = set([
-        tuple([getattr(each, field, '') for field in fields]) for each in
-        session.query(Gdacs).
-            order_by(desc(Gdacs.publisheddate)).
-            with_entities(Gdacs.publisheddate, Gdacs.id).
-            limit(limit)
+        (each.publisheddate, each.id) for each in
+        session.query(GdacsTable).
+            order_by(desc(GdacsTable.c.publisheddate)).
+            with_entities(GdacsTable.c.publisheddate, GdacsTable.c.id)
     ])
 
+    print('Collecting the feeds from GDACS rss feed...', end='\n\n')
     feeds = GDACSFeed().get_feeds()
-    new_feeds = list(filter(lambda feed: tuple([feed[field] for field in fields]) not in latest_100_entries, feeds))
+    new_feeds = list(filter(lambda feed: (feed['publisheddate'], feed['id']) not in latest_100_entries, feeds))
+    print(f'Found {len(new_feeds)} new gdacs rss feeds', end='\n\n')
+
+    engine.execute(GdacsTable.insert(), new_feeds)
